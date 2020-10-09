fe4c81110bb8e77c3a784f8e695d86d7b61b36e4
wehwan <we_hwan@naver.com>
2020-10-09 16:48:12 +0900

    springboot web application setting change.

--- src/main/java/com/example/sample/java8/Application.java
+++ src/main/java/com/example/sample/java8/Application.java
@@ -1,13 +1,21 @@
 package com.example.sample.java8;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+//		SpringApplication.run(Application.class, args);
+		SpringApplication springApplication = new SpringApplication(Application.class);
+		springApplication.setWebApplicationType(WebApplicationType.NONE);
+
+		springApplication.run(args);
+		log.info("spring application started!");
 	}
 
 }
