547f4dd2533705a4979636c302fd9c4cddadb2c0
wehwan <we_hwan@naver.com>
2020-10-09 17:27:17 +0900

    new lamda.class

---
+++ src/main/java/com/example/sample/java8/lamda/SampleLamda.java
@@ -0,0 +1,11 @@
+package com.example.sample.java8.lamda;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SampleLamda {
+
+    public SampleLamda() {
+        log.info("hello lamda!");
+    }
+}
