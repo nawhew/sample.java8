ef8f1e5c5d7273b6d7202f01b67fb954a0330643
wehwan <we_hwan@naver.com>
2020-10-11 20:58:30 +0900

    add sample functional interface

---
+++ src/main/java/com/example/sample/java8/lambda/SampleLambda.java
@@ -0,0 +1,13 @@
+package com.example.sample.java8.lambda;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SampleLambda {
+
+    public SampleLambda() {
+        log.info("hello lamda!");
+    }
+
+
+}
