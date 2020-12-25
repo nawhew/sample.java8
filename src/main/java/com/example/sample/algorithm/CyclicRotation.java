ff3b9e317cd2650ec209834ba467acaf1494da94
wehwan <we_hwan@naver.com>
2020-12-25 16:16:45 +0900

    feat: 배열 요소 순환 회전 추가

---
+++ src/main/java/com/example/sample/algorithm/CyclicRotation.java
@@ -0,0 +1,38 @@
+package com.example.sample.algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CyclicRotation {
+    private static final int ROTATE_DEFAULT_COUNT = 1;
+
+    public static List<Integer> rotate(List<Integer> inputs) {
+        return CyclicRotation.rotate(inputs, CyclicRotation.ROTATE_DEFAULT_COUNT);
+    }
+
+    /**
+     * 배열의 요소를 주어진 수만큼 순환 이동 시킵니다
+     * @param inputs
+     * @param count
+     * @return
+     */
+    public static List<Integer> rotate(List<Integer> inputs, int count) {
+        int inputsSize = inputs.size();
+        List<Integer> outputs = new ArrayList<>(inputsSize);
+
+        for (int i = 0; i < inputsSize; i++) {
+            int index = (i + count - CyclicRotation.ROTATE_DEFAULT_COUNT) % inputsSize;
+            System.out.println("index: " + index);
+            outputs.add(inputs.get(index));
+        }
+
+        return outputs;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CyclicRotation.rotate(
+                Arrays.asList(3, 8, 9, 7, 6), 1
+        ));
+    }
+}
