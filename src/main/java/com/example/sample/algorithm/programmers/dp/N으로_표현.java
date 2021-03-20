ddade68bb440fb15322d6d7cfaca806c239a4960
wehwan <we_hwan@naver.com>
2021-03-20 23:22:36 +0900

    polishing

--- src/main/java/com/example/sample/algorithm/programmers/dp/N으로_표현.java
+++ src/main/java/com/example/sample/algorithm/programmers/dp/N으로_표현.java
@@ -1,4 +1,15 @@
 package com.example.sample.algorithm.programmers.dp;
 
 public class N으로_표현 {
+    public static void main(String[] args) {
+        Solution_N으로_표현 solution = new Solution_N으로_표현();
+        System.out.println(solution.solution(4, 12));
+    }
+}
+
+class Solution_N으로_표현 {
+    public int solution(int N, int number) {
+        int answer = 0;
+        return answer;
+    }
 }
